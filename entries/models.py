0d93aa3003bfb1cafff83f2f894253c07b60455b
Paul Vergeev <paulvergeev@gmail.com>
2017-09-06 17:08:05 +0300

    Add entry model

--- entries/models.py
+++ entries/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Entry(models.Model):
+    title = models.CharField(max_length=96)
+    signature = models.CharField(max_length=64)
+    text = models.TextField()
+    slug = models.SlugField(editable=False)
+    session_key = models.CharField(max_length=40)  # see https://docs.djangoproject.com/en/1.11/topics/http/sessions/#django.contrib.sessions.base_session.AbstractBaseSession.session_key
+    creation_time = models.DateTimeField(auto_now_add=True)
+    modification_time = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title, allow_unicode=True)
+        super(Entry, self).save(*args, **kwargs)
--- telegraph/settings.py
+++ telegraph/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    'entries.apps.EntriesConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
