89cab25fb4e40abcfe6f84a8f2b1e9311891b7a7
Paul Vergeev <paulvergeev@gmail.com>
2017-09-08 15:22:21 +0300

    Prepare for Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn telegraph.wsgi --log-file -
--- telegraph/settings.py
+++ telegraph/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,13 +20,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j_el6or0rc&8npit+=db*!4yo9yxr40%6m!5vw03#cqz0y!tlo'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# SECURITY WARNING: keep the secret key used in production secret!
+if DEBUG:
+    SECRET_KEY = 'j_el6or0rc&8npit+=db*!4yo9yxr40%6m!5vw03#cqz0y!tlo'
+else:
+    SECRET_KEY = os.environ['SECRET_KEY']
+
+
+ALLOWED_HOSTS = ['safe-cove-33456.herokuapp.com']
 
 
 # Application definition
@@ -73,12 +78,17 @@ WSGI_APPLICATION = 'telegraph.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.config()
+    }
 
 
 # Password validation
